fb1d71eca43f8921962c654078dee450f1cb9fb6
Ignacio Calvo <ignacio.calvo@gmail.com>
2024-09-20 13:34:56 +0200

    Try lilypond function with lilynode

---
+++ netlify/functions/lilypond.ts
@@ -0,0 +1,15 @@
+﻿import { Handler } from '@netlify/functions'
+import { render } from 'lilynode'
+export const handler: Handler = async (event, context) => {
+    const svg: Buffer = await render(
+        "\\score {    {    \\clef bass    < c' g\\harmonic >4    }  \\layout {        ragged-right = ##t    \\context {        \\Staff      \\omit TimeSignature      \\omit BarLine        }    }}", // Content of a LilyPond file
+        { format: "svg" },
+    )
+
+    return {
+        statusCode: 200,
+        headers: {'Content-type' : 'image/svg+xml'},
+        body: svg.toString('base64'),
+        isBase64Encoded : true,
+    }
+}
