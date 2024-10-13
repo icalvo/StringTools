1a534b5fcbe8235baab60897abd2bf9b8278df3e
Ignacio Calvo <ignacio.calvo@gmail.com>
2024-10-13 11:54:44 +0200

    First sequence implementation (just take one of each chord fingering)

--- packages/string-fingerings/fingerings.spec.ts
+++ packages/string-fingerings/fingerings.spec.ts
@@ -1,192 +1,253 @@
-﻿import { describe, expect, it } from 'vitest'
-import { abcnote, calculateFingerings, getStopRelPos, hasNoGaps, nn, noteName, noteNumber } from './fingerings.js'
+﻿import { describe, expect, it } from "vitest";
+import {
+  abcnote,
+  calculateFingerings,
+  calculateFingeringSequence,
+  getStopRelPos,
+  hasNoGaps,
+  nn,
+  noteName,
+  noteNumber,
+} from "./fingerings.js";
 
-describe('noteName', () => {
-  it('return C4 for number 60', () => {
-    expect(noteName(60)).toBe('C4')
-  })
-  it('return C#4 for number 61', () => {
-    expect(noteName(61)).toBe('C#4')
-  })
-  it('return B4 for number 59', () => {
-    expect(noteName(59)).toBe('B3')
-  })
-  it('throws for number 4.7', () => {
-    expect(() => noteName(4.7)).toThrow()
-  })
-  it('throws for number -12', () => {
-    expect(() => noteName(-12)).toThrow()
-  })
-})
+describe("noteName", () => {
+  it("return C4 for number 60", () => {
+    expect(noteName(60)).toBe("C4");
+  });
+  it("return C#4 for number 61", () => {
+    expect(noteName(61)).toBe("C#4");
+  });
+  it("return B4 for number 59", () => {
+    expect(noteName(59)).toBe("B3");
+  });
+  it("throws for number 4.7", () => {
+    expect(() => noteName(4.7)).toThrow();
+  });
+  it("throws for number -12", () => {
+    expect(() => noteName(-12)).toThrow();
+  });
+});
 
-describe('noteNumber', () => {
-  it('returns 60 for middle C (C4)', () => {
-    expect(noteNumber('C4')).toBe(60)
-  })
-  it('complies with octave changes between B and C', () => {
-    expect(noteNumber('B3')).toBe(59)
-  })
-  it('recognizes lower case note names', () => {
-    expect(noteNumber('d4')).toBe(62)
-  })
-  it('recognizes sharp alteration', () => {
-    expect(noteNumber('C#4')).toBe(61)
-  })
-  it('recognizes flat alteration', () => {
-    expect(noteNumber('Bb3')).toBe(58)
-  })
-  it('returns error message with empty string', () => {
-    expect(noteNumber('')).to.be.a('string')
-  })
-  it('returns error message if no valid note is given', () => {
-    expect(noteNumber('X5')).toBeTypeOf('string')
-  })
-  it('returns error message if no octave is given', () => {
-    expect(noteNumber('G')).toBeTypeOf('string')
-  })
-  it('returns error message if an invalid alteration is given', () => {
-    expect(noteNumber('Gy6')).toBeTypeOf('string')
-  })
-  it('returns error message if there are extra chars', () => {
-    expect(noteNumber('Gb6!')).toBeTypeOf('string')
-  })
-  it('works with white spaces around', () => {
-    expect(noteNumber('   gb6 ')).toBe(90)
-  })
-})
+describe("noteNumber", () => {
+  it("returns 60 for middle C (C4)", () => {
+    expect(noteNumber("C4")).toBe(60);
+  });
+  it("complies with octave changes between B and C", () => {
+    expect(noteNumber("B3")).toBe(59);
+  });
+  it("recognizes lower case note names", () => {
+    expect(noteNumber("d4")).toBe(62);
+  });
+  it("recognizes sharp alteration", () => {
+    expect(noteNumber("C#4")).toBe(61);
+  });
+  it("recognizes flat alteration", () => {
+    expect(noteNumber("Bb3")).toBe(58);
+  });
+  it("returns error message with empty string", () => {
+    expect(noteNumber("")).to.be.a("string");
+  });
+  it("returns error message if no valid note is given", () => {
+    expect(noteNumber("X5")).toBeTypeOf("string");
+  });
+  it("returns error message if no octave is given", () => {
+    expect(noteNumber("G")).toBeTypeOf("string");
+  });
+  it("returns error message if an invalid alteration is given", () => {
+    expect(noteNumber("Gy6")).toBeTypeOf("string");
+  });
+  it("returns error message if there are extra chars", () => {
+    expect(noteNumber("Gb6!")).toBeTypeOf("string");
+  });
+  it("works with white spaces around", () => {
+    expect(noteNumber("   gb6 ")).toBe(90);
+  });
+});
 
-describe('nn', () => {
-  it('returns the same as noteNumber', () => {
-    expect(nn('D4')).toBe(62)
-  })
+describe("nn", () => {
+  it("returns the same as noteNumber", () => {
+    expect(nn("D4")).toBe(62);
+  });
 
-  it('fails if no valid note is given', () => {
-    expect(() => nn('X5')).toThrow()
-  })
-})
+  it("fails if no valid note is given", () => {
+    expect(() => nn("X5")).toThrow();
+  });
+});
 
-describe('getStopRelPos', () => {
-  it('Returns expected results', () => {
-    expect(getStopRelPos(0)).toBeCloseTo(0)
-    expect(getStopRelPos(7)).toBeCloseTo(0.33258, 3)
-    expect(getStopRelPos(12)).toBeCloseTo(0.5)
-    expect(getStopRelPos(24)).toBeCloseTo(0.75)
-  })
-})
+describe("getStopRelPos", () => {
+  it("Returns expected results", () => {
+    expect(getStopRelPos(0)).toBeCloseTo(0);
+    expect(getStopRelPos(7)).toBeCloseTo(0.33258, 3);
+    expect(getStopRelPos(12)).toBeCloseTo(0.5);
+    expect(getStopRelPos(24)).toBeCloseTo(0.75);
+  });
+});
 
-describe('abcnote', () => {
+describe("abcnote", () => {
   it.each([
-    ['C', 12 * 5],
-    ['^C', (12 * 5) + 1],
-    ['B,', 12 * 5 - 1],
-    ['c', 12 * 6],
-    ['c\'', 12 * 7],
-    ['c\'\'', 12 * 8],
-    ['C,', 12 * 4],
-    ['C,,', 12 * 3]
-  ])('returns %s for %i', (expected, midiNumber) => {
-    expect(abcnote(midiNumber)).toBe(expected)
-  })
-})
+    ["C", 12 * 5],
+    ["^C", 12 * 5 + 1],
+    ["B,", 12 * 5 - 1],
+    ["c", 12 * 6],
+    ["c'", 12 * 7],
+    ["c''", 12 * 8],
+    ["C,", 12 * 4],
+    ["C,,", 12 * 3],
+  ])("returns %s for %i", (expected, midiNumber) => {
+    expect(abcnote(midiNumber)).toBe(expected);
+  });
+});
 
 const violin = {
-  name: 'Violin',
+  name: "Violin",
   stops: 24,
   scaleLength: 330,
-  image: 'violin_front.jpg',
-  clef: 'treble',
+  image: "violin_front.jpg",
+  clef: "treble",
   hardStretch: 78,
   maxStretch: 116,
   strings: [
     {
-      name: 'G',
-      openNote: nn('G3'),
+      name: "G",
+      openNote: nn("G3"),
     },
     {
-      name: 'D',
-      openNote: nn('D4'),
+      name: "D",
+      openNote: nn("D4"),
     },
     {
-      name: 'A',
-      openNote: nn('A4'),
+      name: "A",
+      openNote: nn("A4"),
     },
     {
-      name: 'E',
-      openNote: nn('E5'),
-    }
-  ]
-}
+      name: "E",
+      openNote: nn("E5"),
+    },
+  ],
+};
 
-describe('hasNoGaps', () => {
-  it('Returns expected results', () => {
-    expect(hasNoGaps(undefined, [
-      { stringIndex: 3 },
-      { stringIndex: 4 }])).toBe(true)
-    expect(hasNoGaps(undefined, [
-      { stringIndex: 1 },
-      { stringIndex: 3 }])).toBe(false)
-    expect(hasNoGaps(undefined, [
-      { stringIndex: 1 },
-      { stringIndex: 2 },
-      { stringIndex: 4 }])).toBe(false)
-  })
-})
+describe("hasNoGaps", () => {
+  it("Returns expected results", () => {
+    expect(hasNoGaps(undefined, [{ stringIndex: 3 }, { stringIndex: 4 }])).toBe(
+      true,
+    );
+    expect(hasNoGaps(undefined, [{ stringIndex: 1 }, { stringIndex: 3 }])).toBe(
+      false,
+    );
+    expect(
+      hasNoGaps(undefined, [
+        { stringIndex: 1 },
+        { stringIndex: 2 },
+        { stringIndex: 4 },
+      ]),
+    ).toBe(false);
+  });
+});
 
-describe('calculateFingerings', () => {
-  it('works with single note', () => {
+describe("calculateFingerings", () => {
+  it("works with single note", () => {
     expect(calculateFingerings(violin, [72], [], false)).toStrictEqual([
       [
         {
-          'naturalHarmonic': false,
-          'noteNumber': 72,
-          'stopIndex': 17,
-          'stringIndex': 0
-        }
+          naturalHarmonic: false,
+          noteNumber: 72,
+          stopIndex: 17,
+          stringIndex: 0,
+        },
       ],
       [
         {
-          'naturalHarmonic': false,
-          'noteNumber': 72,
-          'stopIndex': 10,
-          'stringIndex': 1
-        }
+          naturalHarmonic: false,
+          noteNumber: 72,
+          stopIndex: 10,
+          stringIndex: 1,
+        },
       ],
       [
         {
-          'naturalHarmonic': false,
-          'noteNumber': 72,
-          'stopIndex': 3,
-          'stringIndex': 2
-        }
-      ]
-    ])
-  })
-  it('works with quadruple stop C-major chord', () => {
-    expect(calculateFingerings(violin, [60, 67, 72, 76], [], false)).toStrictEqual([
-      [{
-        'stringIndex': 0,
-        'noteNumber': 60,
-        'naturalHarmonic': false,
-        'stopIndex': 5
-      },
+          naturalHarmonic: false,
+          noteNumber: 72,
+          stopIndex: 3,
+          stringIndex: 2,
+        },
+      ],
+    ]);
+  });
+  it("works with quadruple stop C-major chord", () => {
+    expect(
+      calculateFingerings(violin, [60, 67, 72, 76], [], false),
+    ).toStrictEqual([
+      [
         {
-          'stringIndex': 1,
-          'noteNumber': 67,
-          'naturalHarmonic': false,
-          'stopIndex': 5
+          stringIndex: 0,
+          noteNumber: 60,
+          naturalHarmonic: false,
+          stopIndex: 5,
         },
         {
-          'stringIndex': 2,
-          'noteNumber': 72,
-          'naturalHarmonic': false,
-          'stopIndex': 3
+          stringIndex: 1,
+          noteNumber: 67,
+          naturalHarmonic: false,
+          stopIndex: 5,
         },
         {
-          'stringIndex': 3,
-          'noteNumber': 76,
-          'naturalHarmonic': false,
-          'stopIndex': 0
-        }]
-    ])
-  })
-})
+          stringIndex: 2,
+          noteNumber: 72,
+          naturalHarmonic: false,
+          stopIndex: 3,
+        },
+        {
+          stringIndex: 3,
+          noteNumber: 76,
+          naturalHarmonic: false,
+          stopIndex: 0,
+        },
+      ],
+    ]);
+  });
+});
+
+describe("calculateFingeringSequences", () => {
+  it("works with semitone slide", () => {
+    expect(
+      calculateFingeringSequence(
+        violin,
+        [
+          [60, 67],
+          [61, 68],
+        ],
+        [],
+        false,
+      ),
+    ).toStrictEqual([
+      [
+        {
+          stringIndex: 0,
+          noteNumber: 60,
+          naturalHarmonic: false,
+          stopIndex: 5,
+        },
+        {
+          stringIndex: 1,
+          noteNumber: 67,
+          naturalHarmonic: false,
+          stopIndex: 5,
+        },
+      ],
+      [
+        {
+          stringIndex: 0,
+          noteNumber: 61,
+          naturalHarmonic: false,
+          stopIndex: 6,
+        },
+        {
+          stringIndex: 1,
+          noteNumber: 68,
+          naturalHarmonic: false,
+          stopIndex: 6,
+        },
+      ],
+    ]);
+  });
+});
--- packages/string-fingerings/fingerings.ts
+++ packages/string-fingerings/fingerings.ts
@@ -1,4 +1,4 @@
-﻿import type { Instrument, InstrumentString, Stop } from './types.js'
+﻿import type { Instrument, InstrumentString, Stop } from "./types.js";
 
 /**
  * Gets the MIDI note number from a text representation (middle C is 'C4')
@@ -6,87 +6,99 @@
  * @returns The MIDI note number or a string with an explanatory error message.
  */
 export function noteNumber(noteName: string): number | string {
-  noteName = noteName.trim()
-  if (noteName.length < 2) return 'Note names must be at least two characters long'
-  let pointer = 0
-  const firstChar = noteName.charAt(pointer).toUpperCase().charCodeAt(0)
-  if (firstChar < 65 || firstChar > 65 + 7) return 'First char must be ABCDEFG'
+  noteName = noteName.trim();
+  if (noteName.length < 2)
+    return "Note names must be at least two characters long";
+  let pointer = 0;
+  const firstChar = noteName.charAt(pointer).toUpperCase().charCodeAt(0);
+  if (firstChar < 65 || firstChar > 65 + 7) return "First char must be ABCDEFG";
 
-  const noteIndex = (firstChar - 67 + 7) % 7
-  const semitoneIndex = [0, 2, 4, 5, 7, 9, 11]
+  const noteIndex = (firstChar - 67 + 7) % 7;
+  const semitoneIndex = [0, 2, 4, 5, 7, 9, 11];
 
-  pointer++
-  let alteration = 0
-  if (noteName.charAt(pointer) === '#') {
-    alteration = 1
-    pointer++
-  } else if (noteName.charAt(pointer) === 'b') {
-    alteration = -1
-    pointer++
+  pointer++;
+  let alteration = 0;
+  if (noteName.charAt(pointer) === "#") {
+    alteration = 1;
+    pointer++;
+  } else if (noteName.charAt(pointer) === "b") {
+    alteration = -1;
+    pointer++;
   }
-  const octave = parseInt(noteName.charAt(pointer))
-  if (isNaN(octave)) return 'Last char must be a number'
-  pointer++
-  if (pointer !== noteName.length) return 'Note name has extra characters'
+  const octave = parseInt(noteName.charAt(pointer));
+  if (isNaN(octave)) return "Last char must be a number";
+  pointer++;
+  if (pointer !== noteName.length) return "Note name has extra characters";
 
-  return (semitoneIndex[noteIndex] ?? NaN) + alteration + (octave + 1) * 12
+  return (semitoneIndex[noteIndex] ?? NaN) + alteration + (octave + 1) * 12;
 }
 
 export function nn(noteName: string) {
-    const n = noteNumber(noteName)
-    if (typeof n === 'string') throw `Invalid note name: ${n}`
-    return n
+  const n = noteNumber(noteName);
+  if (typeof n === "string") throw `Invalid note name: ${n}`;
+  return n;
 }
 
 export function noteName(noteNumber: number): string {
   if (!Number.isSafeInteger(noteNumber) || noteNumber < 0 || noteNumber > 127) {
-    throw "Invalid note number"
+    throw "Invalid note number";
   }
 
-  const noteIndex = noteNumber % 12
-  const noteName = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'][noteIndex] ?? ""
-  const octave = Math.floor((noteNumber / 12)) - 1
-  return noteName + octave
+  const noteIndex = noteNumber % 12;
+  const noteName =
+    ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"][
+      noteIndex
+    ] ?? "";
+  const octave = Math.floor(noteNumber / 12) - 1;
+  return noteName + octave;
 }
 
 export function abcnote(noteNumber: number): string {
-    const noteIndex = noteNumber % 12
-    let noteName = ['c', 'c', 'd', 'd', 'e', 'f', 'f', 'g', 'g', 'a', 'a', 'b'][noteIndex] ?? ""
-    const alteration = ['', '^', '', '^', '', '', '^', '', '^', '', '^', ''][noteIndex] ?? ""
-    const octave = Math.floor(noteNumber / 12)
-    let ticks: number
-    if (octave <= 5) {
-        noteName = noteName.toUpperCase()
-        ticks = 5 - octave
-        return `${alteration}${noteName}${','.repeat(ticks)}`
-    } else {
-        ticks = octave - 6
-        return `${alteration}${noteName}${"'".repeat(ticks)}`
-    }
+  const noteIndex = noteNumber % 12;
+  let noteName =
+    ["c", "c", "d", "d", "e", "f", "f", "g", "g", "a", "a", "b"][noteIndex] ??
+    "";
+  const alteration =
+    ["", "^", "", "^", "", "", "^", "", "^", "", "^", ""][noteIndex] ?? "";
+  const octave = Math.floor(noteNumber / 12);
+  let ticks: number;
+  if (octave <= 5) {
+    noteName = noteName.toUpperCase();
+    ticks = 5 - octave;
+    return `${alteration}${noteName}${",".repeat(ticks)}`;
+  } else {
+    ticks = octave - 6;
+    return `${alteration}${noteName}${"'".repeat(ticks)}`;
+  }
 }
 
 function* stopsForString<TString extends InstrumentString>(
   instrument: Instrument<TString>,
   stringIndex: number,
   noteNumber: number,
-  includeNaturalHarmonics: boolean
+  includeNaturalHarmonics: boolean,
 ) {
-  console.debug(`Calculating stop for note ${noteNumber} on string ${stringIndex}`)
-  const instrumentString = instrument.strings[stringIndex]
+  console.debug(
+    `Calculating stop for note ${noteNumber} on string ${stringIndex}`,
+  );
+  const instrumentString = instrument.strings[stringIndex];
   if (!instrumentString) return;
-  const openNote = instrumentString.openNote as number
-  const stopIndex = noteNumber - openNote
+  const openNote = instrumentString.openNote as number;
+  const stopIndex = noteNumber - openNote;
 
   if (stopIndex < 0) {
-    console.debug(`${noteName(noteNumber)} is too low for ${instrumentString.name} string`)
-    return
+    console.debug(
+      `${noteName(noteNumber)} is too low for ${instrumentString.name} string`,
+    );
+    return;
   }
 
   if (stopIndex > instrument.stops) {
-    console.debug(`${noteName(noteNumber)} is too high to stop for ${instrumentString.name} string`)
-  }
-  else {
-    yield { stringIndex, noteNumber, stopIndex, naturalHarmonic: false }
+    console.debug(
+      `${noteName(noteNumber)} is too high to stop for ${instrumentString.name} string`,
+    );
+  } else {
+    yield { stringIndex, noteNumber, stopIndex, naturalHarmonic: false };
   }
 
   if (includeNaturalHarmonics) {
@@ -98,17 +110,17 @@ function* stopsForString<TString extends InstrumentString>(
       { partial: 4, touch: 12 + 7 + 5, produces: 12 + 7 + 5 },
       { partial: 5, touch: 4, produces: 12 + 7 + 5 + 4 },
       { partial: 5, touch: 12 + 4, produces: 12 + 7 + 5 + 4 },
-      { partial: 5, touch: 12 + 7 + 5 + 4, produces: 12 + 7 + 5 + 4 }
-    ]
+      { partial: 5, touch: 12 + 7 + 5 + 4, produces: 12 + 7 + 5 + 4 },
+    ];
 
     yield* naturalHarmonics
       .map((h) => ({
         stringIndex,
         stopIndex: h.touch,
         noteNumber: h.produces + openNote,
-        naturalHarmonic: true
+        naturalHarmonic: true,
       }))
-      .filter((h) => h.noteNumber === noteNumber)
+      .filter((h) => h.noteNumber === noteNumber);
   }
 }
 
@@ -117,106 +129,143 @@ function* stopsForString<TString extends InstrumentString>(
  * @param stopIndex semitone index (number of semitones above the open string)
  */
 export function getStopRelPos(stopIndex: number): number {
-  return 1 - Math.pow(2, -stopIndex / 12)
+  return 1 - Math.pow(2, -stopIndex / 12);
 }
 
 function stopsForInstrument<TString extends InstrumentString>(
   instrument: Instrument<TString>,
   notes: number[],
-  includeNaturalHarmonics: boolean
+  includeNaturalHarmonics: boolean,
 ): Stop[][] {
   const result: Stop[][] = notes.map((note) =>
     instrument.strings
       .flatMap((_s, stringIndex) => [
-        ...stopsForString(instrument, stringIndex, note, includeNaturalHarmonics)
+        ...stopsForString(
+          instrument,
+          stringIndex,
+          note,
+          includeNaturalHarmonics,
+        ),
       ])
-      .filter((f) => f !== null)
-  )
+      .filter((f) => f !== null),
+  );
 
   if (result.some((r) => r.length === 0)) {
-    return []
+    return [];
   }
 
-  return result
+  return result;
 }
 
 function cross(addValidation: Function, l1: Stop[][], l2: Stop[]): Stop[][] {
   if (l1.length === 0) {
-    return l2.map((l) => [l])
+    return l2.map((l) => [l]);
   }
 
-  return l1.flatMap((i1) => l2.filter((i2) => addValidation(i1, i2)).map((i2) => i1.concat([i2])))
+  return l1.flatMap((i1) =>
+    l2.filter((i2) => addValidation(i1, i2)).map((i2) => i1.concat([i2])),
+  );
 }
 
-function fingeringStretch<TString extends InstrumentString>(instrument: Instrument<TString>, stop1: Stop, stop2: Stop): number {
-  const stopRelPos1 = getStopRelPos(stop1.stopIndex)
-  const stopRelPos2 = getStopRelPos(stop2.stopIndex)
-  return instrument.scaleLength * Math.abs(stopRelPos2 - stopRelPos1)
+function fingeringStretch<TString extends InstrumentString>(
+  instrument: Instrument<TString>,
+  stop1: Stop,
+  stop2: Stop,
+): number {
+  const stopRelPos1 = getStopRelPos(stop1.stopIndex);
+  const stopRelPos2 = getStopRelPos(stop2.stopIndex);
+  return instrument.scaleLength * Math.abs(stopRelPos2 - stopRelPos1);
 }
 
 declare global {
   interface Array<T> {
-    pairwise(): Array<[T, T]>
+    pairwise(): Array<[T, T]>;
   }
 }
 
 Array.prototype.pairwise = function () {
-  return this.slice(1).map((x, i) => [this[i], x])
-}
+  return this.slice(1).map((x, i) => [this[i], x]);
+};
 
-function fingeringStretches<TString extends InstrumentString>(instrument: Instrument<TString>, stops: Stop[]): [Stop, number, Stop][] {
+function fingeringStretches<TString extends InstrumentString>(
+  instrument: Instrument<TString>,
+  stops: Stop[],
+): [Stop, number, Stop][] {
   return stops
     .pairwise()
-    .map(([stop1, stop2]) => [stop1, fingeringStretch(instrument, stop1, stop2), stop2])
+    .map(([stop1, stop2]) => [
+      stop1,
+      fingeringStretch(instrument, stop1, stop2),
+      stop2,
+    ]);
 }
 
 function isOpenString(stop: Stop): boolean {
-  return stop.stopIndex === 0
+  return stop.stopIndex === 0;
 }
 
 function stretchHardness<TString extends InstrumentString>(
   instrument: Instrument<TString>,
   stop1: Stop,
   stretch: number,
-  stop2: Stop
+  stop2: Stop,
 ): number {
   if (isOpenString(stop1) || isOpenString(stop2)) {
-    return 0.0
+    return 0.0;
   }
 
   if (stretch > instrument.maxStretch) {
-    return 1.0
+    return 1.0;
   }
 
   if (stretch > instrument.hardStretch) {
-    return 0.5
+    return 0.5;
   }
 
-  return 0.1
+  return 0.1;
 }
 
-function stretchesHardness<TString extends InstrumentString>(instrument: Instrument<TString>, stretches: [Stop, number, Stop][]): number {
+function stretchesHardness<TString extends InstrumentString>(
+  instrument: Instrument<TString>,
+  stretches: [Stop, number, Stop][],
+): number {
   return stretches
-    .map(([stop1, stretch, stop2]) => stretchHardness(instrument, stop1, stretch, stop2))
-    .reduce((a, b) => Math.max(a, b))
+    .map(([stop1, stretch, stop2]) =>
+      stretchHardness(instrument, stop1, stretch, stop2),
+    )
+    .reduce((a, b) => Math.max(a, b));
 }
 
-function fingeringHardness<TString extends InstrumentString>(instrument: Instrument<TString>, fingering: Stop[]): number {
-  const sortedFingering = fingering.sort((s1, s2) => s1.stringIndex - s2.stringIndex)
+function fingeringHardness<TString extends InstrumentString>(
+  instrument: Instrument<TString>,
+  fingering: Stop[],
+): number {
+  const sortedFingering = fingering.sort(
+    (s1, s2) => s1.stringIndex - s2.stringIndex,
+  );
 
-  return stretchesHardness(instrument, fingeringStretches(instrument, sortedFingering))
+  return stretchesHardness(
+    instrument,
+    fingeringStretches(instrument, sortedFingering),
+  );
 }
 
-export function hasNoGaps(_instrument: any, fingering: {stringIndex: number}[]): boolean {
+export function hasNoGaps(
+  _instrument: any,
+  fingering: { stringIndex: number }[],
+): boolean {
   return fingering
     .map((stop) => stop.stringIndex)
     .sort()
     .pairwise()
-    .every(([str1, str2]) => Math.abs(str1 - str2) <= 1)
+    .every(([str1, str2]) => Math.abs(str1 - str2) <= 1);
 }
 
-export function hasPossibleStretch<TString extends InstrumentString>(instrument: Instrument<TString>, fingering: Stop[]): boolean {
-  return fingeringHardness(instrument, fingering) !== 1.0
+export function hasPossibleStretch<TString extends InstrumentString>(
+  instrument: Instrument<TString>,
+  fingering: Stop[],
+): boolean {
+  return fingeringHardness(instrument, fingering) !== 1.0;
 }
 
 /**
@@ -230,26 +279,58 @@ export function calculateFingerings<TString extends InstrumentString>(
   instrument: Instrument<TString>,
   notes: number[],
   validations: Function[],
-  includeNaturalHarmonics: boolean = false
+  includeNaturalHarmonics: boolean = false,
 ): Stop[][] {
-  console.debug(`Calculating fingerings for ${notes} on ${instrument.name}`)
+  console.debug(`Calculating fingerings for ${notes} on ${instrument.name}`);
 
-  const stopsByNote = stopsForInstrument(instrument, notes, includeNaturalHarmonics)
+  const stopsByNote = stopsForInstrument(
+    instrument,
+    notes,
+    includeNaturalHarmonics,
+  );
 
   if (notes.length === 1) {
-    return stopsByNote.flatMap(noteStops => noteStops.map(s => [s]))
+    return stopsByNote.flatMap((noteStops) => noteStops.map((s) => [s]));
   }
 
-  const init: Stop[][] = []
+  const init: Stop[][] = [];
   return stopsByNote
     .reduce((acc, stops) => cross(stringIsNotRepeated, acc, stops), init)
-    .filter(validation)
+    .filter(validation);
 
   function stringIsNotRepeated(stopList: Stop[], stop: Stop): boolean {
-    return stopList.every((existingStop) => existingStop.stringIndex !== stop.stringIndex)
+    return stopList.every(
+      (existingStop) => existingStop.stringIndex !== stop.stringIndex,
+    );
   }
 
   function validation(fng: Stop[]): boolean {
-    return validations.map((val) => val(instrument, fng)).every((result) => result)
+    return validations
+      .map((val) => val(instrument, fng))
+      .every((result) => result);
   }
+}
+
+export function calculateFingeringSequence<TString extends InstrumentString>(
+  instrument: Instrument<TString>,
+  chords: number[][],
+  validations: Function[],
+  includeNaturalHarmonics: boolean = false,
+): Stop[][] | undefined {
+  let result = [];
+  for (const chord of chords) {
+    const fingerings = calculateFingerings(
+      instrument,
+      chord,
+      validations,
+      includeNaturalHarmonics,
+    );
+    if (fingerings[0]) {
+      result.push(fingerings[0]);
+    } else {
+      return undefined;
+    }
+  }
+
+  return result;
 }
